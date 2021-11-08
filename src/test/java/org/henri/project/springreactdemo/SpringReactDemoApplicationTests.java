8f012b21f7d692431c2c6c458bf50134ad740af6
henriguy1210 <henri.idrovo@pm.me>
2021-11-08 14:38:43 -0600

    initial commit

---
+++ src/test/java/org/henri/project/springreactdemo/SpringReactDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.henri.project.springreactdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
